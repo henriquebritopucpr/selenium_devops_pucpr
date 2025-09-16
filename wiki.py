6d877ee551a0cb47123f6f6630b7df3491acb507
enirq <enirqbirto@gmail.com>
2025-09-15 23:00:46 -0300

    Implementa busca por 'Immanuel Kant' na Wikipedia

--- wiki.py
+++ wiki.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 if __name__ == "__main__":
     driver = webdriver.Chrome()
     driver.get("https://www.wikipedia.org")
-    driver.quit()
 
+    search_box = driver.find_element(By.ID, "searchInput")
+    search_box.send_keys("Immanuel Kant")
+    search_box.send_keys(Keys.RETURN)
+
+    driver.quit()
